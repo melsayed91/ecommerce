b3acc73493d35977f3a3d2d13d29666ecf70aed3
MohamedAlaa <m.elsayed.alaa@gmail.com>
2018-01-27 01:58:44 +0400

    -- toggle control in settings

--- american.app.FE/src/app/account/settings/settings.component.ts
+++ american.app.FE/src/app/account/settings/settings.component.ts
@@ -7,6 +7,7 @@ import { UserService } from '../../core/services/user.service/user.service';
 })
 export class SettingsComponent implements OnInit {
   userAccount;
+  blacklistCustomers;
   constructor(private auth: UserService) {
     this.userAccount = this.auth.account;
   }
