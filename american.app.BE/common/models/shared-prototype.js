766c4db98ce8d154d1b70ae63d28d29049aac7c7
MohamedAlaa <m.elsayed.alaa@gmail.com>
2018-02-14 22:12:36 +0400

    -- prototype data models

---
+++ american.app.BE/common/models/shared-prototype.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function (prototype) {
+    // status prototype [init , accepted/rejected , in progress ,payment pending,paied, start shiping , shipped ,deleivered/downloaded , closed ]
+};
