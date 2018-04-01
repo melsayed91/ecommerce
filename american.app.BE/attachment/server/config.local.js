e301d4e8191a9a4772bac85cc7334137ba3f15b0
MohamedAlaa <m.elsayed.alaa@gmail.com>
2018-04-01 19:01:10 +0400

    -- fix attachment , syscode, quation folder

--- american.app.BE/attachment/server/config.local.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const path = require('path')
-var GLOBAL_CONFIG = require(path.join(__dirname, '../../common/global.config'));
-module.exports = {
-    host: GLOBAL_CONFIG.hostname
-};
